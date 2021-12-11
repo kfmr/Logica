137f1b21dc64a70071f932bfb05500ae4d59f760
Karina Moura <57039290+kfmr@users.noreply.github.com>
2021-12-11 17:15:19 -0300

    added js solved problems

---
+++ JavaScript/FCC/desafios/falsy-bouncer.js
@@ -0,0 +1,15 @@
+function bouncer(arr) {
+ return arr.filter(value => value)
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+//tradicional
+// function bouncer(arr) {
+//   newArr = []
+//   for(value of arr){
+//     if(value)
+//       newArr.push(value)
+//   }
+//   return newArr
+// }
